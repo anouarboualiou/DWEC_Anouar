5787fc7a84c52a934901b7bcd17917f8c18644e5
anouarboualiou <anwarboualiou@gmail.com>
2026-05-22 10:25:39 +0200

    cambios de bd en ejercicios 18 y 19

--- Ejerc_18/config/db.js
+++ Ejerc_18/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'ejercicio18.c8clwpsat3yg.us-east-1.rds.amazonaws.com',
+  host: 'ejercicio18.cdkmmgm80owy.us-east-1.rds.amazonaws.com',
   user: 'admin',
   password: 'anouaranouar',
   database: 'ejercicio18'
